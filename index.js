be47391b1e61cc4e9c87cd71f6890429c4ec0387
psytoh <uuk65536@gmail.com>
2025-01-10 02:04:17 +0900

    カウントダウンdivを削除

--- index.js
+++ index.js
@@ -35,31 +35,32 @@ class Queue {
 
 const STOCK_NUM = 5;
 const ready_btn = document.getElementById("readyBtn");
-const wait_div = document.getElementById("waitDiv");
 const example_div = document.getElementById("exampleDiv");
 const input_div = document.getElementById("inputDiv");
 
 ready_btn.addEventListener("click", async () => {
-    ready_btn.style.display = "none";
-    wait_div.style.display = "block";
+    ready_btn.clickable = false;
+    ready_btn.textContent = "準備中...";
     await fetchExampleText();
     
-    wait_div.textContent = "3";
+    ready_btn.style.display = "none";
+    input_div.style.visibility = "visible";
+    example_div.style.visibility = "visible";
+    example_div.children[1].textContent = "3";
 
     setTimeout(() => {
-        wait_div.textContent = "2";
+        example_div.children[1].textContent = "2";
     }, 1000)
     setTimeout(() => {
-        wait_div.textContent = "1";
+        example_div.children[1].textContent = "1";
     }, 2000);
     setTimeout(() => {
-        wait_div.textContent = "0";
+        example_div.children[1].textContent = "0";
     }, 3000);
     setTimeout(() => {
-        wait_div.style.display = "none";
-        example_div.style.visibility = "visible";
+        example_div.style.textAlign = "left";
+        loadNextExampleText();
     }, 4000);
-    await loadNextExampleText();
 });
 
 let is_composing = false; // IME入力が確定されていないかどうかを示すフラグ
@@ -80,26 +81,28 @@ input_div.addEventListener("input", assessTypingProgress);
 function assessTypingProgress() {
     if (is_composing) return; // IME入力が確定されていないときは処理を中断
 
-    let i = 0;
-    for (; i < input_div.value.length; i++) {
-        if (input_div.value[i] == exampleText[i]) {
+    let match_length = 0;
+    for (; match_length < input_div.value.length; match_length++) {
+        if (input_div.value[match_length] == exampleText[match_length]) {
             continue;
         } else {
             break;
         }
     }
-    if (i == exampleText.length) {
+    if (match_length == exampleText.length) {
         loadNextExampleText();
     } else {
-        example_div.children[0].textContent = exampleText.slice(0, i);
-        example_div.children[1].textContent = exampleText.slice(i, exampleText.length);
+        example_div.children[0].textContent = exampleText.slice(0, match_length);
+        example_div.children[1].textContent = exampleText.slice(match_length, exampleText.length);
     }
 };
 
 
 let example_queue = new Queue();
 
-// 次の例文を表示
+/**
+ * 次の例文を読み込む
+ */
 async function loadNextExampleText() {
     do {
         exampleText = example_queue.pop();
@@ -117,6 +120,9 @@ async function loadNextExampleText() {
     }
 }
 
+/**
+ * 例文を取得する
+ */
 async function fetchExampleText() {
     await fetch(`/application/generate?count=${STOCK_NUM}`)
         .then(response => response.text())
@@ -126,6 +132,7 @@ async function fetchExampleText() {
             for (let i = 0; i < text_arr.length; i++) {
                 example_queue.push(text_arr[i]);
             }
+            console.log(example_queue);
         })
         .catch(error => {
             console.error('Error during Fetching:', error);
