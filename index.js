c23eb125e0c92939e70224622739fc2f28e10663
psytoh <uuk65536@gmail.com>
2025-01-10 10:19:51 +0900

    コメントを追加、編集

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * @description キュー構造を配列で実現するクラス
+ * キュー構造を配列で実現するクラス
  * @function enqueue - 一つの要素をキューに追加
  * @function dequeue - 一つの要素をキューから取り出す
  * @property {number} length - キューの長さ
@@ -16,8 +16,8 @@ class Queue {
         return this._queue.length;
     }
 
-    /**
-     * 要素を末尾に追加する
+    /**^
+     * キューに要素を入れる
      * @param {*} element - プッシュする要素
      */
     enqueue(element) {
@@ -25,7 +25,7 @@ class Queue {
     }
 
     /**
-     * 先頭の要素を取り出す
+     * キューから要素を取り出す
      * @returns {*} 先頭の要素を返す／キューが空の場合はnullを返す
      */
     dequeue() {
@@ -36,8 +36,9 @@ class Queue {
     }
 }
 
-// 定数
-const STOCK_NUM = 20; // 例文のストック数
+const STOCK_TARGET = 20; // お題のストック数
+
+// DOM要素
 const ready_btn = document.getElementById("readyBtn");
 const preview_div = document.getElementById("previewDiv");
 const example_div = document.getElementById("exampleDiv");
@@ -46,6 +47,7 @@ const input_div = document.getElementById("inputDiv");
 let is_ready_btn_clicked = false; // 準備ボタンがクリックされたかどうかを示すフラグ
 // 準備ボタンがクリックされたときに呼び出される
 ready_btn.addEventListener("click", async () => {
+    // 一回目以降のクリックは無視
     if (is_ready_btn_clicked) return;
     is_ready_btn_clicked = true;
 
@@ -57,6 +59,7 @@ ready_btn.addEventListener("click", async () => {
     example_div.style.visibility = "visible";
     example_div.children[1].textContent = "3";
 
+    // カウントダウン
     setTimeout(() => {
         example_div.children[1].textContent = "2";
     }, 1000)
@@ -86,17 +89,19 @@ input_div.addEventListener('compositionend', () => {
     assessTypingProgress();
 });
 
-// 例文を格納する変数
+// お題を格納する変数
 let example_text = "";
 let example_queue = new Queue();
+
 /**
- * 入力された文字列と例文を比較し、入力状況を表示する
+ * 入力された文字列とお題を比較し、入力状況を表示する
  */
 function assessTypingProgress() {
     if (is_composing) return; // IME入力が確定されていないときは処理を中断
 
     let match_length = 0;
     let temp_text = example_div.innerText;
+    // 一致している文字数を数える
     for (; match_length < input_div.value.length; match_length++) {
         if (input_div.value[match_length] == temp_text[match_length]) {
             continue;
@@ -104,50 +109,55 @@ function assessTypingProgress() {
             break;
         }
     }
-    if (match_length == temp_text.length) {
-        loadNextExampleText();
-    } else {
+
+    if (match_length == temp_text.length) { // お題をクリアしたとき
+        loadNextExampleText(); // 次のお題を読み込む
+    } else { // お題をクリアしていないとき
         example_div.children[0].textContent = temp_text.slice(0, match_length);
         example_div.children[1].textContent = temp_text.slice(match_length, temp_text.length);
     }
 };
 
 /**
- * 次の例文を読み込む
+ * 次のお題を読み込む
  */
 async function loadNextExampleText(is_first = false) {
-    if (example_queue.length < STOCK_NUM / 2) {
+    // お題が`STOCK_NUM`の半分以下になったら非同期で追加取得
+    if (example_queue.length < STOCK_TARGET / 2) {
         fetchExampleText();
     }
+    // 入力欄をクリア
     input_div.value = "";
 
-    if (is_first) {
+    // お題を読み込む
+    if (is_first) { // 初回のみ
         example_text = example_queue.dequeue();
         example_div.children[0].textContent = "";
         example_div.children[1].textContent = example_text;
         example_text = example_queue.dequeue();
         preview_div.children[0].textContent = "";
         preview_div.children[1].textContent = example_text;
-    } else {
+    } else { // 2回目以降
         example_text = example_queue.dequeue();
         example_div.children[0].textContent = "";
         example_div.children[1].textContent = preview_div.children[1].textContent;
-
         preview_div.children[0].textContent = "";
         preview_div.children[1].textContent = example_text;
     }
 }
 
 /**
- * 例文を取得する
+ * お題を `count` 個取得してキューに追加する
+ * @param {number} count - 取得するお題の数 (デフォルト: `STOCK_NUM`)
  */
-async function fetchExampleText(_count = null) {
-    await fetch(`/application/generate?count=${_count ? _count : STOCK_NUM}`)
+async function fetchExampleText(count = null) {
+    await fetch(`/application/generate?count=${count ? count : STOCK_TARGET}`)
         .then(response => response.text())
         .then(data => {
             let result = data;
             // 改行で分割して配列に格納
             let text_arr = result.split('\n').filter(text => text.trim() !== "");
+            // お題をキューに追加
             for (let i = 0; i < text_arr.length; i++) {
                 example_queue.enqueue(text_arr[i]);
             }
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const fs = require("fs");
 const path = require('path');
 const { GoogleGenerativeAI } = require("@google/generative-ai"); // Google Generative AI SDKをインポート
 const URL = require("url");
+
+// ポート番号
 const PORT = 3000;
 
+// MIMEタイプ
 const MIME_TYPE = {
     '.html': 'text/html',
     '.js': 'text/javascript',
@@ -15,16 +18,21 @@ const MIME_TYPE = {
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY); // APIキーを設定してインスタンス化
 
+/**
+ * Gemini-1.5-flashモデルを使って指定された数の文を生成する
+ * @param {number} count - 生成する文の数
+ * @returns {string} - 生成された文
+ */
 async function generate(count) {
     const model = genAI.getGenerativeModel({model : "gemini-1.5-flash"}); // モデルを指定
     
     const prompt = `日本語の一つの文を${count}個生成せよ。日本語の文以外を出力した場合ペナルティがあります。生成する文は、多様な文体を交えること。生成した文と文の間には改行以外入れない事。`; // プロンプトを設定
 
-    console.log("Generating content...");
+    // console.log("Generating content...");
     
     try {
         const result = await model.generateContent(prompt);
-        console.log("Content generated successfully!");
+        // console.log("Content generated successfully!");
 
         const response = result.response;
         const text = response.text();
@@ -35,13 +43,15 @@ async function generate(count) {
     }
 }
 
+// サーバーを作成
 const server = http.createServer(async function (request, response) {
-    const parsedUrl = URL.parse(request.url, true);
+    const parsedUrl = URL.parse(request.url, true); // URLをパース
 
     // /application/generateにGETリクエストが来たら、generate関数を呼び出し、結果を返す
     if (parsedUrl.pathname === '/application/generate' && request.method === 'GET') {
         const count = parseInt(parsedUrl.query.count) || 0; // クエリパラメータからcountを取得
         try {
+            // 文を生成
             const result = await generate(count);
             response.writeHead(200, { 'Content-Type': 'text/plain' });
             response.end(result);
@@ -50,7 +60,6 @@ const server = http.createServer(async function (request, response) {
             response.writeHead(500, { 'Content-Type': 'text/plain' });
             response.end('生成中にエラーが発生しました');
         }
-
         return;
     }
 
@@ -60,6 +69,7 @@ const server = http.createServer(async function (request, response) {
         filePath = './index.html';
     }
 
+    // ファイルの拡張子に応じてMIMEタイプを設定 (デフォルトはapplication/octet-stream)
     const extname = String(path.extname(filePath)).toLowerCase();
     const contentType = MIME_TYPE[extname] || 'application/octet-stream';
 
@@ -67,21 +77,25 @@ const server = http.createServer(async function (request, response) {
     fs.readFile(filePath, function (error, content) {
             if (error) {
                 if (error.code === 'ENOENT') { // Error NO ENTry / ファイルが存在しない
+                    // 404ページを返す
                     fs.readFile('./404.html', (_error, page404) => {
                         response.writeHead(404, { 'Content-Type': 'text/html' });
                         response.end(page404, 'utf-8');
                     });
                 } else {
+                    // その他のエラー
                     response.writeHead(500);
                     response.end(`Server Error: ${error.code}`);
                 }
             } else {
+                // ファイルを正常に読み込んだ場合
                 response.writeHead(200, { 'Content-Type': contentType });
                 response.end(content, 'utf-8');
             }
         });
 });
 
+// サーバーを起動
 server.listen(PORT, () => {
     if (process.env.API_KEY === undefined) {
         console.log("API Key is not set. Please set the API_KEY environment variable.");
